5b8778cdb3ddcef1cbca0c1902dd7297fe83236b
ppdx999 <ppdx999@gmail.com>
2021-12-12 14:24:27 +0900

    modified

--- setup.sh
+++ setup.sh
@@ -13,58 +13,16 @@ case $PATH in :*) PATH=${PATH#?};; esac
 IFS=$(printf ' \t\n_'); IFS={IFS%_}
 export IFS
 
-# === Define the commonly used and useful functions ===================
-
-error_exit() {
-	echo "$0: ${1:-"Unknown Error"}" 1>&2
-	exit 1
-}
-
-print_usage_and_exit() {
-  cat <<-USAGE 1>&2
-		USAGE        :  open_browser file ... 
-		Description  :  make it easier to open the browser from the console 
-	USAGE
-  exit 1
-}
-
-detectOS() {
-   case "$(uname -s)" in
-   	Linux*)     echo Linux;;
-   	Darwin*)    echo Mac;;
-   	CYGWIN*)    echo Cygwin;;
-   	MINGW*)     echo MinGw;;
-	MSYS*)      echo MSYS;;
-   	*)          echo "UNKNOWN:$(uname -s)" ;;
-   esac
-}
-
-###############################################################################
-# Main Routine 
-###############################################################################
 # === Main Routine ===================================================
 
-case "$(detectOS)" in
-	'Linux' | 'Mac' )
+case "$(uname -s)" in
+	Linux*)
 		ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 		ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
 		ln -sf $HOME/dotfiles/.vim $HOME/.vim
 		;;
-	'MSYS' )
-		case $( sed -n -e 's/^rem set MSYS=winsymlinks:nativestrict$/set MSYS=winsymlinks:nativestrict/' -e 's/^rem set MSYS2_PATH_TYPE=inherit$/set MSYS2_PATH_TYPE=inherit/' /msys2_shell.cmd ) in 
-			'' ) 
-				ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-				ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-				ln -sf $HOME/dotfiles/.vim $HOME/.vim
-				;;
-			*)
-				sed -e 's/^rem set MSYS=winsymlinks:nativestrict$/set MSYS=winsymlinks:nativestrict/' -e 's/^rem set MSYS2_PATH_TYPE=inherit$/set MSYS2_PATH_TYPE=inherit/' /msys2_shell.cmd > "/msys2_shell.cmd.$(date +'%Y%m%d')"
-				mv "/msys2_shell.cmd.$(date +'%Y%m%d')" /msys2_shell.cmd
-				echo 'Re-Execute setup.sh as administrator'
-        exit
-				;;
-		esac
+	*)    
+		echo "Error: Unsupported OS or System: $(uname -s)"  1>&2
+		exit 1
 		;;
-	* )
-		error_exit "Unsupported OS or System"
 esac
