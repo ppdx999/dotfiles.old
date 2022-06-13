9379a96eae6917ea2f99e1798c8f0de94e8b88b6
ppdx999 <ppdx999@gmail.com>
2022-06-13 11:04:36 +0900

    configure for arch linux

--- setup.sh
+++ setup.sh
@@ -1,28 +1,5 @@
 #! /bin/sh
 
-# === Initialize shell environment ===================================
-set -eu
-unset -f unalias
-\unalias -a
-unset -f command
-if command -v umask >/dev/null 2>&1; then umask 0022; fi
-export LC_ALL=C
-export PATH="$(command -p getconf PATH 2>/dev/null)${PATH+:}${PATH-}"
-case $PATH in :*) PATH=${PATH#?};; esac
-#export UNIX_STD=2003 # to make HP-UX conform to POSIX
-IFS=$(printf ' \t\n_'); IFS={IFS%_}
-export IFS
-
-# === Main Routine ===================================================
-
-case "$(uname -s)" in
-	Linux*)
-		ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-		ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-		ln -sf $HOME/dotfiles/.vim $HOME/.vim
-		;;
-	*)    
-		echo "Error: Unsupported OS or System: $(uname -s)"  1>&2
-		exit 1
-		;;
-esac
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.vim $HOME/.vim
+ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
